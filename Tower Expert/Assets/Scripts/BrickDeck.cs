38157eabc4740891629638f9795c9f32663dee0e
Yun Jiang (RIT Student) <yj9812@ad.rit.edu>
2019-03-01 17:19:43 -0500

    Finish Draw Bricks

--- Tower Expert/Assets/Scripts/BrickDeck.cs
+++ Tower Expert/Assets/Scripts/BrickDeck.cs
@@ -2,20 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BrickDeck : Deck {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Material[] Materials;
+    public Brick[] Bricks;
+    private int randomBrickColor;
+    //private ParticleSystem particle;
+    // Use this for initialization
+    void Start () {
+        InitBrick();
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     public void Shuffle()
     {
+
+    }
+    public void OnDraw(GameObject brick)
+    {
+        randomBrickColor = Random.Range(0, 3);
+        
+        MeshRenderer brickRenderer = brick.GetComponent<MeshRenderer>();
+        brickRenderer.material = Materials[randomBrickColor];
+        //particle = brick.GetComponentInChildren<ParticleSystem>();
+        //particle.Play();
+    }
+
+    void InitBrick()
+    {
+        randomBrickColor = Random.Range(0, 3);
+        Bricks[0].SetColor(randomBrickColor);
+        MeshRenderer brickRenderer = Bricks[0].gameObject.GetComponent<MeshRenderer>();
+        brickRenderer.material = Materials[randomBrickColor];
+
+        randomBrickColor = Random.Range(0, 3);
+        Bricks[1].SetColor(randomBrickColor);
+        brickRenderer = Bricks[1].gameObject.GetComponent<MeshRenderer>();
+        brickRenderer.material = Materials[randomBrickColor];
+
+        randomBrickColor = Random.Range(0, 3);
+        Bricks[2].SetColor(randomBrickColor);
+        brickRenderer = Bricks[2].gameObject.GetComponent<MeshRenderer>();
+        brickRenderer.material = Materials[randomBrickColor];
+
 
     }
 
