64a2052dd835a48a9296c27301cb390304d36f19
Frisk Jiang <728501875@qq.com>
2019-02-28 22:51:46 -0500

    Add GridBoardManager

---
+++ Tower Expert/Assets/Scripts/GridBoardManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBoardManager : MonoBehaviour {
+
+    public Brick[][] bricks;
+
+    private int brickCount;
+    
+	// Use this for initialization
+	void Start () {
+        brickCount = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlaceBrickAt(Brick brick, int colum) {
+        brickCount++;
+    }
+
+    public void GetBrickMatrix(int row, int colum) {
+        
+    }
+
+}
