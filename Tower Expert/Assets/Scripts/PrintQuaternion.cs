c6a380d76fe9f562c857902a76c14aac0aa13d15
jj2269 <jj2269@IGM2000-23>
2019-04-03 16:00:09 -0400

    Add quaternion printer

---
+++ Tower Expert/Assets/Scripts/PrintQuaternion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintQuaternion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(gameObject.name + "  Local Quaternion: " + transform.localRotation.ToString("F6"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
